5d82a3c21cbd07e4553ec1bccf005a20c718185e
Luis Palacios <lpalac4@gmail.com>
2022-09-20 15:19:51 -0400

    Updated dependencies

--- app/src/main/java/com/rightpoint/emojipuzzler/puzzle/PuzzleFragment.kt
+++ app/src/main/java/com/rightpoint/emojipuzzler/puzzle/PuzzleFragment.kt
@@ -11,46 +11,47 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.rightpoint.emojipuzzler.R
-import kotlinx.android.synthetic.main.puzzle_fragment.*
+import com.rightpoint.emojipuzzler.databinding.PuzzleFragmentBinding
 
 class PuzzleFragment : Fragment() {
 
     private lateinit var viewModel: PuzzleViewModel
+    private var _binding: PuzzleFragmentBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(activity!!.application).create(PuzzleViewModel::class.java)
-
-        viewModel.currentEmojiPuzzle.observe(this, Observer {
-            emoji_container.text = it
-        })
-        viewModel.feedBackLiveData.observe(this, Observer {
-            active_feedback.text = it
-        })
-        viewModel.loading.observe(this, Observer {
-            progress_bar.visibility = if(it) View.VISIBLE else View.GONE
-        })
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application).create(PuzzleViewModel::class.java)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = PuzzleFragmentBinding.inflate(inflater, container, false)
 
+        viewModel.currentEmojiPuzzle.observe(viewLifecycleOwner, Observer {
+            _binding?.emojiContainer?.text = it
+        })
+        viewModel.feedBackLiveData.observe(viewLifecycleOwner, Observer {
+            _binding?.activeFeedback?.text = it
+        })
+        viewModel.loading.observe(viewLifecycleOwner, Observer {
+            _binding?.progressBar?.visibility = if(it) View.VISIBLE else View.GONE
+        })
         return inflater.inflate(R.layout.puzzle_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        idle_start_button.setOnClickListener {
-            idle_container.visibility = View.GONE
-            active_container.visibility = View.VISIBLE
+        _binding?.idleStartButton?.setOnClickListener {
+            _binding?.idleContainer?.visibility = View.GONE
+            _binding?.activeContainer?.visibility = View.VISIBLE
             viewModel.startGame()
         }
 
-        emoji_container.setOnClickListener {
+        _binding?.emojiContainer?.setOnClickListener {
             viewModel.newEmoji()
         }
 
-        active_answer.editText?.setOnEditorActionListener { textView, actionId, event ->
+        _binding?.activeAnswer?.editText?.setOnEditorActionListener { textView, actionId, event ->
             if (actionId == EditorInfo.IME_NULL
                 && event.action == KeyEvent.ACTION_DOWN) {
                 Log.v("EmojiEntry", textView.text.toString())
